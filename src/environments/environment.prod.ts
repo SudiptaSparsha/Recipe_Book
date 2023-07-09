591148ce9ef08d2bfad7c2f67fcfdd79f07a43ac
Sudipta <sudipta.anupam.datta@dohatec.com.bd>
2023-07-09 14:13:59 +0600

    We are just before the NgRx thing now

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production : true,
+    firebaseAPIKey : 'AIzaSyBP5uGOEB2tXj6uOIiRSlu4Oy8L85XB3ps'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production : false,
+    firebaseAPIKey : 'AIzaSyBP5uGOEB2tXj6uOIiRSlu4Oy8L85XB3ps'
+};
